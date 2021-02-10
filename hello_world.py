f8e05eb112735e40cf977b5e9152fe004e497410
drivendragon <drivendragon@hotmail.com>
2021-02-09 23:59:57 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+print("Hello World!")
+
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+
+
+
+
+name = "Zen"
+print("My name is", name)
+
+
+name = "Zen"
+print("My name is " + name)
+
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+x = "hello world"
+print(x.title())
+# output:
+"Hello World"
+
+
