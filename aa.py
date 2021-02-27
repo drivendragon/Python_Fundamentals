de3064f02e5f13f97ca0dc7d4a8426856cf0ef3a
drivendragon <drivendragon@hotmail.com>
2021-02-27 14:41:05 -0500

    functionBasicII updates

---
+++ aa.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def length_and_value(size,value):
+#    newList = []
+#    for i in range(size):
+#        newList.append(value)
+#    return newList
+#print(length_and_value(3,8))
